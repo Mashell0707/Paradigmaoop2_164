a47651c5559e5fb97d475330684fc0cb887894dd
Mashell0707 <merlsutama@gmail.com>
2023-05-29 14:58:27 +0700

    membuat class orang

--- paradigmaoop2_164/paradigmaoop2_164/paradigmaoop2_164.cpp
+++ paradigmaoop2_164/paradigmaoop2_164/paradigmaoop2_164.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
+
+class manusia {
+public:
+	string jeniskelamin;
+
+	manusia(string pJenisKelamin) :
+		jeniskelamin(pJenisKelamin) {
+		cout << "manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "manusia dihapus\n" << endl;
+	}
+};
+
